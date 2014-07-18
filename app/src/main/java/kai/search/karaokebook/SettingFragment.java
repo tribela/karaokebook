2f6773d092b593157225880a8143a89fb1da1852
kjwon15 <kjwonmail@gmail.com>
2014-07-18 09:29:04 +0900

    Check update mockup.

--- app/src/main/java/kai/search/karaokebook/SettingFragment.java
+++ app/src/main/java/kai/search/karaokebook/SettingFragment.java
@@ -3,12 +3,15 @@ package kai.search.karaokebook;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingFragment extends PreferenceFragment {
+public class SettingFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener {
+
+    private static final String KEY_UPDATE = "update";
 
 
     public SettingFragment() {
@@ -19,5 +22,16 @@ public class SettingFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_general);
+
+        Preference update = findPreference(KEY_UPDATE);
+        update.setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        if (preference.getKey() == KEY_UPDATE) {
+            // TODO: check update.
+        }
+        return false;
     }
 }
